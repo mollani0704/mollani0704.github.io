6e6f2e3713092b592b356f22ed1327da70d0a895
mollani0704 <mollani0704@gmail.com>
2026-03-05 23:33:13 +0900

    [Update] Header 수정

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  author: '문종성',
+  headerTitle: '문몰래의 코딩노트',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
