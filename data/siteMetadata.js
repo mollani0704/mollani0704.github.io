e9a95bb3b179d883ea02a2288b0b6d147507711a
mollani0704 <mollani0704@gmail.com>
2026-03-31 23:02:06 +0900

    [Update] 브라우저 Tab title 문구 변경

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: '문몰래의 코딩노트',
   author: '문종성',
   headerTitle: '문몰래의 코딩노트',
   description: 'A blog created with Next.js and Tailwind.css',
